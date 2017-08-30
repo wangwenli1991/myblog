ef41b8eb1b8f14c3cf7ba04f857080fbaf85f4c9
wangwenli1991 <1527179450@qq.com>
2017-08-30 08:52:28 +0800

    layouts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,1 +1,7 @@
-@extends('layouts.master')
+@extends('layout')
+
+@section('content')
+
+asd
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/posts','PostsController@index');
+
+Route::get('/','PostsController@index');
